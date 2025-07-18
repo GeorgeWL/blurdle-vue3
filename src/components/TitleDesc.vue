5bcd4e3bdf0f5d8f40ef484ad3671fb774a3a56b
GeorgeWL <georgelangham1990@gmail.com>
2025-07-18 23:04:03 +0100

    improved styling titledesc

--- src/components/TitleDesc.vue
+++ src/components/TitleDesc.vue
@@ -1,7 +1,11 @@
 <template>
-  <div class="greetings">
-    <h1 class="title">Blurdle</h1>
-    <p>
+  <div class="title-desc">
+    <div class="title-wrapper">
+
+      <h1 class="title">Blurdle</h1>
+      <h3>The Blurry guessing game</h3>
+    </div>
+    <p class="description">
       After each guess the image will become less pixelated and blurred, each time a one word Proper Noun
       <br />(cat, dog, house, cardigan, train, etc)
     </p>
@@ -14,22 +18,24 @@ h1 {
   font-weight: 500;
   font-size: 2.6rem;
   position: relative;
-  top: -10px;
 }
 
 h3 {
   font-size: 1.2rem;
 }
 
-.greetings h1,
-.greetings h3 {
+.title-desc h1,
+.title-desc h3 {
   text-align: center;
+}
+.title-wrapper {
+  margin-bottom: 2rem;
 }
 
 @media (min-width: 1024px) {
 
-  .greetings h1,
-  .greetings h3 {
+  .title-desc h1,
+    .title-desc h3 {
     text-align: left;
   }
 }
